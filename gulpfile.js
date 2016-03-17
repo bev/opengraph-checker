571526fd726dcde3fcf41f30e350f3b511e1392c
Beverly Cheak <beverly.cheak@hotmail.com>
2016-03-17 15:16:45 +1100

    Start integrating Gulp into project

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,43 @@
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	sass = require('gulp-ruby-sass'),
+	pleeease = require('gulp-pleeease'),
+	sourcemaps = require('gulp-sourcemaps'),
+	csscomb = require('gulp-csscomb');
 
-// Gulp Tasks
+/* Options */
+var sassOptions = {
+	style: 'expanded'
+};
+
+var pleeeaseOptions = { // set minifier to false to keep Sass sourcemaps support
+	optimizers: {
+		minifier: false
+	}
+};
+
+/* Tasks */
+gulp.task('css', function () {
+	return sass('./src/css/*.scss', sassOptions)
+	.on('error', sass.logError)
+	.pipe(sourcemaps.write('maps', {
+		includeContent: false,
+		sourceRoot: 'source'
+	}))
+	.pipe(pleeease(pleeeaseOptions))
+	.pipe(csscomb()) // CSSComb isn't working
+	.pipe(gulp.dest('./dist/css'));
+});
+
+// My important watch task
+gulp.task('watch', function(){
+	gulp.watch('./src/css/*.scss', ['css']); 
+})
+
 gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
+	return gutil.log('Gulp is running!')
 });
 
 gulp.task('hello', function() {
-  console.log('Hello World');
+	console.log('Hello World');
 });
